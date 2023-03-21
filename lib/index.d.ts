13139246d6dd4c2d8be0f7fc1c57fcb5a64fb1be
palari <fahadsaleem020@gmail.com>
2023-03-22 00:35:57 +0500

    fix: updated deps
    updated deps

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,7 +2,7 @@ export * from "@chakra-editor/buttons";
 export * from "@chakra-editor/components";
 export * from "@chakra-editor/extensions";
 export * from "@chakra-editor/functions";
-export * from "@chakra-editor/hooks";
+export { useForm, useStorage, useTable } from "@chakra-editor/hooks";
 export * from "@chakra-editor/provider";
 export * from "@chakra-editor/toolbar";
 export * from "@chakra-editor/types";
